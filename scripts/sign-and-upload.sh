29d5b1962ead2395f6f2fa9a13370c3ca40bc9a1
wangxinyu <wangxinyu@malalaoshi.com>
2016-09-27 10:14:37 +0800

    IOS-461 modify code sign info for build

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -9,7 +9,7 @@
 # fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
-OUTPUTDIR="$PWD/build/Release-iphoneos"
+OUTPUTDIR="$PWD/build/Debug-iphoneos"
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
