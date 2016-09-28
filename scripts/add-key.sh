c2e43ba78d41214aa70aa5293febb75d21e47f63
wangxinyu <wangxinyu@malalaoshi.com>
2016-09-28 17:27:18 +0800

    IOS-461 modify .travis.yml

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -2,9 +2,9 @@
 echo "***************************"
 echo "*     Security Import     *"
 echo "***************************"
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
-security security list-keychain
+security list-keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/login.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/login.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
