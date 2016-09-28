0a2e64f208fbcbb67c074c0bea8955501bc596ed
wangxinyu <wangxinyu@malalaoshi.com>
2016-09-28 16:50:41 +0800

    IOS-461 modify build script

--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+echo "***************************"
+echo "*     Security Delete     *"
+echo "***************************"
 security delete-keychain ios-build.keychain
 rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision
