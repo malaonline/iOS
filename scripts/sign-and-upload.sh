f6bbba0d78811d1272c2a247be38b2a5a2d332ee
wangxinyu <wangxinyu@malalaoshi.com>
2016-09-30 10:30:39 +0800

    IOS-461 update version to 2.3.1

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,12 +1,12 @@
 # #!/bin/sh
-# if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-#   echo "This is a pull request. No deployment will be done."
-#   exit 0
-# fi
-# if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-#   echo "Testing on a branch other than master. No deployment will be done."
-#   exit 0
-# fi
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Debug-iphoneos"
