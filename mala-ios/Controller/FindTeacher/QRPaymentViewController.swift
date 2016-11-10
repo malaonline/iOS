bb4928b68e98b72c0881404432f710c9067458f5
wangxinyu <wangxinyu@malalaoshi.com>
2016-11-10 15:43:13 +0800

    IOS-477 Add CRCode Support file, QRPaymentViewController.

--- mala-ios/Controller/FindTeacher/QRPaymentViewController.swift
+++ mala-ios/Controller/FindTeacher/QRPaymentViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class QRPaymentViewController: UIViewController {
 
+    // MARK: - Components
+    
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupUserInterface()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+ 
+    // MARK: - Private Method
+    private func setupUserInterface() {
+        // Style
+        
+        // SubViews
+        
+        // Autolayout
     }
-    */
-
 }
