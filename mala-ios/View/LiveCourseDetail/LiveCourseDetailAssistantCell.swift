4133b2558ebbd4df04195cc3c156439ca0f38d75
wangxinyu <wangxinyu@malalaoshi.com>
2016-11-21 14:23:59 +0800

    IOS-492 Add LiveCourseDetailAssistantCell

---
+++ mala-ios/View/LiveCourseDetail/LiveCourseDetailAssistantCell.swift
@@ -0,0 +1,21 @@
+//
+//  LiveCourseDetailAssistantCell.swift
+//  mala-ios
+//
+//  Created by 王新宇 on 2016/11/21.
+//  Copyright © 2016年 Mala Online. All rights reserved.
+//
+
+import UIKit
+
+class LiveCourseDetailAssistantCell: MalaBaseLiveCourseCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
