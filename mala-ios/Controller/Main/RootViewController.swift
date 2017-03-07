4f805dd6a5498fac52cc8bfd2e58d0663cb89f0d
Elors <ElorsAt@iCloud.com>
2017-03-07 18:18:33 +0800

    IOS-540: Remove local file and manage Proposer using Cocoapods.

--- mala-ios/Controller/Main/RootViewController.swift
+++ mala-ios/Controller/Main/RootViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Proposer
 import PagingMenuController
 
 private struct PagingMenuOptions: PagingMenuControllerCustomizable {
--- mala-ios/Tool/MalaAlert.swift
+++ mala-ios/Tool/MalaAlert.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import Proposer
+import Proposer
 
 class MalaAlert {
 
