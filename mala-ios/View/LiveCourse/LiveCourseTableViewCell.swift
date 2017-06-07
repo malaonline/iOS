712e04f4eeb4a38b80307c15b6a297089f95266e
Elors <ElorsAt@iCloud.com>
2017-06-07 17:24:42 +0800

    IOS-636: Live-course card visual optimization.

--- mala-ios/View/LiveCourse/LiveCourseTableViewCell.swift
+++ mala-ios/View/LiveCourse/LiveCourseTableViewCell.swift
@@ -19,7 +19,7 @@ class LiveCourseTableViewCell: UITableViewCell {
             lecturerAvatar.setImage(withURL: model.lecturerAvatar)
             assistantAvatar.setImage(withURL: model.assistantAvatar)
             lecturerNameLabel.text = model.lecturerName
-            assistantNameLabel.text = String(format: "助教:%@", (model.assistantName ?? 0))
+            assistantNameLabel.text = String(format: "助教 %@", (model.assistantName ?? 0))
             lecturerTitleLabel.text = model.lecturerTitle
             
             courseName.text = model.courseName
@@ -32,76 +32,109 @@ class LiveCourseTableViewCell: UITableViewCell {
     
     
     // MARK: - Components
-    /// 卡片容器
+    /// card container
     private lazy var content: UIView = {
         let view = UIView(UIColor.white)
-        view.addShadow(color: UIColor(named: .ShadowGray))
+        view.addShadow(color: UIColor(named: .liveShadowBlue))
         return view
     }()
-    /// 教师信息布局容器
+    /// teacher-info container
     private lazy var teacherContent: UIView = {
-        let view = UIView(UIColor(named: .LiveClassCardBlue))
+        let view = UIView(UIColor(named: .liveThemeBlue))
         view.clipsToBounds = true
         return view
     }()
-    /// 课程信息布局容器
-    private lazy var courseContent: UIView = {
-        let view = UIView(UIColor.white)
-        return view
-    }()
-    /// 老师头像
-    private lazy var lecturerAvatar: UIImageView = {
-        let imageView = UIImageView(
-            frame: CGRect(x: 0, y: 0, width: 64, height: 64),
-            cornerRadius: 32,
-            image: "avatar_placeholder"
-        )
-        return imageView
-    }()
-    /// 老师头像背景
-    private lazy var lecturerAvatarBackground: UIView = {
-        let view = UIView(UIColor(named: .LiveAvatarBack), cornerRadius: 35)
-        return view
-    }()
-    /// 直播图标
-    private lazy var liveIcon: UIImageView = {
-        let imageView = UIImageView(imageName: "live_icon")
-        return imageView
-    }()
-    /// 助教头像
-    private lazy var assistantAvatar: UIImageView = {
-        let imageView = UIImageView(
-            frame: CGRect(x: 0, y: 0, width: 54, height: 54),
-            cornerRadius: 27,
-            image: "avatar_placeholder"
-        )
-        return imageView
-    }()
-    /// 助教头像背景
-    private lazy var assistantAvatarBackground: UIView = {
-        let view = UIView(UIColor(named: .LiveAvatarBack), cornerRadius: 29)
-        return view
-    }()
-    /// 老师姓名
-    private lazy var lecturerNameLabel: UILabel = {
+    /// live-course title
+    private lazy var courseName: UILabel = {
         let label = UILabel(
-            text: "主讲老师",
-            font: FontFamily.PingFangSC.Regular.font(17),
-            textColor: UIColor.white
+            text: "课程名称",
+            font: FontFamily.PingFangSC.Regular.font(18),
+            textColor: UIColor(named: .labelBlack)
         )
         return label
     }()
-    /// 老师title
+    /// course state
+    private lazy var courseState: UIImageView = {
+        let imageView = UIImageView(image: UIImage(asset: .preEnrollment))
+        return imageView
+    }()
+    /// subject label
+    private lazy var subjectLabel: UILabel = {
+        let label = UILabel(
+            text: "科",
+            font: FontFamily.PingFangSC.Regular.font(12),
+            textColor: UIColor(named: .liveMathGreen),
+            textAlignment: .center,
+            borderColor: UIColor(named: .liveMathGreen),
+            borderWidth: 1.0,
+            cornerRadius: 2
+        )
+        return label
+    }()
+    /// live-course type
+    private lazy var classTypeLabel: UILabel = {
+        let label = UILabel(
+            text: "暑期班",
+            font: FontFamily.PingFangSC.Regular.font(12),
+            textColor: UIColor(named: .liveCourseTypeBlue),
+            textAlignment: .center,
+            borderColor: UIColor(named: .liveCourseTypeBlue),
+            borderWidth: 1.0,
+            cornerRadius: 2
+        )
+        return label
+    }()
+    /// lecturer-avatar
+    private lazy var lecturerAvatar: UIImageView = {
+        let imageView = UIImageView(
+            frame: CGRect(x: 0, y: 0, width: 82, height: 82),
+            cornerRadius: 41,
+            image: "avatar_placeholder"
+        )
+        return imageView
+    }()
+    /// lecturer-avatar background
+    private lazy var lecturerAvatarBackground: UIView = {
+        let view = UIView(UIColor(named: .LiveAvatarBack), cornerRadius: 44)
+        return view
+    }()
+    /// assistant-avatar
+    private lazy var assistantAvatar: UIImageView = {
+        let imageView = UIImageView(
+            frame: CGRect(x: 0, y: 0, width: 50, height: 50),
+            cornerRadius: 25,
+            image: "avatar_placeholder"
+        )
+        return imageView
+    }()
+    /// assistant-avatar background
+    private lazy var assistantAvatarBackground: UIView = {
+        let view = UIView(UIColor(named: .LiveAvatarBack), cornerRadius: 28)
+        return view
+    }()
+    /// lecturer-name
+    private lazy var lecturerNameLabel: UILabel = {
+        let label = UILabel(
+            text: "主讲老师",
+            font: FontFamily.PingFangSC.Regular.font(18),
+            textColor: UIColor.white,
+            textAlignment: .right
+        )
+        return label
+    }()
+    /// lecturer-intro
     private lazy var lecturerTitleLabel: UILabel = {
         let label = UILabel(
             text: "主讲老师主要成就",
             font: FontFamily.PingFangSC.Light.font(14),
             textColor: UIColor.white,
-            opacity: 0.56
+            textAlignment: .right,
+            opacity: 0.8
         )
+        label.numberOfLines = 0
         return label
     }()
-    /// 助教姓名
+    /// assistant-name
     private lazy var assistantNameLabel: UILabel = {
         let label = UILabel(
             text: "助教",
@@ -110,66 +143,40 @@ class LiveCourseTableViewCell: UITableViewCell {
         )
         return label
     }()
-    private lazy var avatarLine1: UIView = {
-        let view = UIView(UIColor.white)
-        return view
-    }()
-    private lazy var avatarLine2: UIView = {
-        let view = UIView(UIColor.white)
-        return view
-    }()
-    /// 课程名称
-    private lazy var courseName: UILabel = {
-        let label = UILabel(
-            text: "课程名称",
-            font: FontFamily.PingFangSC.Light.font(17),
-            textColor: UIColor(named: .ArticleText)
-        )
-        return label
-    }()
-    /// 课程日期图标
-    private lazy var dateIcon: UIImageView = {
-        let imageView = UIImageView(imageName: "live_time")
-        return imageView
-    }()
-    /// 课程日期
+    /// course-data label
     private lazy var courseDateLabel: UILabel = {
         let label = UILabel(
             text: "课程日期",
-            font: FontFamily.PingFangSC.Light.font(12),
-            textColor: UIColor(named: .ArticleSubTitle)
+            font: FontFamily.PingFangSC.Regular.font(12),
+            textColor: UIColor(named: .protocolGary)
         )
         return label
     }()
-    /// 课程年级信息
+    /// grade
     private lazy var gradeLabel: UILabel = {
         let label = UILabel(
             text: "课程年级区间",
-            font: FontFamily.PingFangSC.Light.font(12),
-            textColor: UIColor(named: .OptionSelectColor),
-            textAlignment: .center,
-            borderColor: UIColor(named: .OptionSelectColor),
-            borderWidth: MalaScreenOnePixel,
-            cornerRadius: 2
+            font: FontFamily.PingFangSC.Regular.font(12),
+            textColor: UIColor(named: .protocolGary)
         )
         return label
     }()
-    /// 价格标签
+    /// price
     private lazy var priceLabel: UILabel = {
         let label = UILabel(
             text: "价格/",
             font: FontFamily.PingFangSC.Light.font(20),
-            textColor: UIColor(named: .ThemeRed),
+            textColor: UIColor(named: .livePriceRed),
             textAlignment: .right
         )
         return label
     }()
-    /// 课时数标签
+    /// period
     private lazy var lessionsLabel: UILabel = {
         let label = UILabel(
             text: "次数",
             font: FontFamily.PingFangSC.Light.font(14),
-            textColor: UIColor(named: .ThemeRed),
+            textColor: UIColor(named: .livePriceRed),
             textAlignment: .right
         )
         return label
@@ -197,131 +204,119 @@ class LiveCourseTableViewCell: UITableViewCell {
         // SubViews
         contentView.addSubview(content)
         content.addSubview(teacherContent)
-        content.addSubview(courseContent)
+        content.addSubview(subjectLabel)
+        content.addSubview(classTypeLabel)
+        content.addSubview(courseName)
+        content.addSubview(courseState)
+        
         teacherContent.addSubview(lecturerAvatar)
         teacherContent.insertSubview(lecturerAvatarBackground, belowSubview: lecturerAvatar)
-        teacherContent.addSubview(liveIcon)
         teacherContent.addSubview(assistantAvatar)
         teacherContent.insertSubview(assistantAvatarBackground, belowSubview: assistantAvatar)
         teacherContent.addSubview(lecturerNameLabel)
-        teacherContent.addSubview(assistantNameLabel)
         teacherContent.addSubview(lecturerTitleLabel)
-        teacherContent.addSubview(avatarLine1)
-        teacherContent.addSubview(avatarLine2)
-        courseContent.addSubview(courseName)
-        courseContent.addSubview(dateIcon)
-        courseContent.addSubview(courseDateLabel)
-        courseContent.addSubview(gradeLabel)
-        courseContent.addSubview(lessionsLabel)
-        courseContent.addSubview(priceLabel)
+        teacherContent.addSubview(assistantNameLabel)
+        
+        content.addSubview(courseDateLabel)
+        content.addSubview(gradeLabel)
+        content.addSubview(priceLabel)
+        content.addSubview(lessionsLabel)
         
         // Autolayout
         content.snp.makeConstraints { (maker) -> Void in
             maker.top.equalTo(contentView).offset(4)
-            maker.left.equalTo(contentView).offset(12)
+            maker.left.equalTo(contentView).offset(10)
             maker.bottom.equalTo(contentView).offset(-4)
-            maker.right.equalTo(contentView).offset(-12)
+            maker.right.equalTo(contentView).offset(-10)
         }
         teacherContent.snp.makeConstraints { (maker) in
-            maker.top.equalTo(content)
+            maker.top.equalTo(content).offset(45)
             maker.left.equalTo(content)
+            maker.bottom.equalTo(content).offset(-38)
             maker.right.equalTo(content)
         }
-        courseContent.snp.makeConstraints { (maker) in
-            maker.top.equalTo(teacherContent.snp.bottom)
-            maker.left.equalTo(content)
-            maker.right.equalTo(content)
-            maker.bottom.equalTo(content)
+        subjectLabel.snp.makeConstraints { (maker) in
+            maker.top.equalTo(content).offset(12)
+            maker.left.equalTo(content).offset(12)
+            maker.height.equalTo(20)
+            maker.width.equalTo(20)
+        }
+        classTypeLabel.snp.makeConstraints { (maker) in
+            maker.top.equalTo(content).offset(12)
+            maker.left.equalTo(subjectLabel.snp.right).offset(6)
+            maker.height.equalTo(20)
+            maker.width.equalTo(45)
+        }
+        courseName.snp.makeConstraints { (maker) in
+            maker.centerY.equalTo(classTypeLabel)
+            maker.left.equalTo(classTypeLabel.snp.right).offset(8)
+            maker.right.equalTo(courseState.snp.left).offset(-10)
+            maker.height.equalTo(25)
+        }
+        courseState.snp.makeConstraints { (maker) in
+            maker.height.equalTo(36)
+            maker.width.equalTo(50)
+            maker.right.equalTo(content).offset(-8)
+            maker.top.equalTo(content).offset(-3)
         }
         lecturerAvatarBackground.snp.makeConstraints { (maker) in
-            maker.top.equalTo(teacherContent).offset(12)
-            maker.centerX.equalTo(teacherContent.snp.right).multipliedBy(0.25)
-            maker.width.equalTo(70)
-            maker.height.equalTo(70)
+            maker.centerX.equalTo(content.snp.right).multipliedBy(0.48)
+            maker.centerY.equalTo(content.snp.bottom).multipliedBy(0.48)
+            maker.width.equalTo(88)
+            maker.height.equalTo(88)
         }
         lecturerAvatar.snp.makeConstraints { (maker) in
             maker.center.equalTo(lecturerAvatarBackground)
-            maker.width.equalTo(64)
-            maker.height.equalTo(64)
-        }
-        liveIcon.snp.makeConstraints { (maker) in
-            maker.top.equalTo(teacherContent).offset(18)
-            maker.right.equalTo(lecturerAvatar).offset(10)
-            maker.width.equalTo(24)
-            maker.height.equalTo(18)
+            maker.width.equalTo(82)
+            maker.height.equalTo(82)
         }
         assistantAvatarBackground.snp.makeConstraints { (maker) in
-            maker.centerY.equalTo(lecturerAvatar)
-            maker.centerX.equalTo(teacherContent.snp.right).multipliedBy(0.75)
-            maker.width.equalTo(58)
-            maker.height.equalTo(58)
+            maker.bottom.equalTo(lecturerAvatarBackground)
+            maker.left.equalTo(lecturerAvatarBackground.snp.right).offset(-6)
+            maker.width.equalTo(56)
+            maker.height.equalTo(56)
         }
         assistantAvatar.snp.makeConstraints { (maker) in
             maker.center.equalTo(assistantAvatarBackground)
-            maker.width.equalTo(54)
-            maker.height.equalTo(54)
+            maker.width.equalTo(50)
+            maker.height.equalTo(50)
         }
         lecturerNameLabel.snp.makeConstraints { (maker) in
-            maker.centerX.equalTo(lecturerAvatar)
-            maker.top.equalTo(lecturerAvatar.snp.bottom).offset(8)
-            maker.height.equalTo(17)
-            maker.bottom.equalTo(lecturerTitleLabel.snp.top).offset(-6)
-        }
-        assistantNameLabel.snp.makeConstraints { (maker) in
-            maker.top.equalTo(assistantAvatar.snp.bottom).offset(8)
-            maker.centerX.equalTo(assistantAvatar)
-            maker.height.equalTo(14)
+            maker.right.equalTo(lecturerAvatarBackground.snp.left).offset(-6)
+            maker.height.equalTo(25)
+            maker.centerY.equalTo(lecturerAvatarBackground)
         }
         lecturerTitleLabel.snp.makeConstraints { (maker) in
-            maker.top.equalTo(lecturerNameLabel.snp.bottom).offset(6)
-            maker.centerX.equalTo(lecturerNameLabel)
-            maker.height.equalTo(14)
-            maker.bottom.equalTo(teacherContent).offset(-12)
+            maker.right.equalTo(lecturerNameLabel)
+            maker.left.equalTo(teacherContent).offset(22)
+            maker.top.equalTo(lecturerNameLabel).offset(6)
+            maker.bottom.equalTo(teacherContent)
         }
-        avatarLine1.snp.makeConstraints { (maker) in
-            maker.height.equalTo(MalaScreenOnePixel)
-            maker.width.equalTo(teacherContent).multipliedBy(0.168)
-            maker.centerX.equalTo(teacherContent).offset(-20)
-            maker.centerY.equalTo(lecturerAvatar).offset(-5)
-        }
-        avatarLine2.snp.makeConstraints { (maker) in
-            maker.height.equalTo(MalaScreenOnePixel)
-            maker.width.equalTo(teacherContent).multipliedBy(0.168)
-            maker.centerX.equalTo(teacherContent).offset(20)
-            maker.centerY.equalTo(lecturerAvatar).offset(5)
-        }
-        courseName.snp.makeConstraints { (maker) in
-            maker.top.equalTo(courseContent).offset(17)
-            maker.left.equalTo(courseContent).offset(12)
-            maker.height.equalTo(17)
-            maker.bottom.equalTo(dateIcon.snp.top).offset(-12)
-        }
-        dateIcon.snp.makeConstraints { (maker) in
-            maker.width.equalTo(10)
-            maker.height.equalTo(10)
-            maker.top.equalTo(courseName.snp.bottom).offset(12)
-            maker.left.equalTo(courseContent).offset(12)
-            maker.bottom.equalTo(courseContent).offset(-18)
+        assistantNameLabel.snp.makeConstraints { (maker) in
+            maker.height.equalTo(20)
+            maker.left.equalTo(assistantAvatarBackground.snp.right).offset(1)
+            maker.right.equalTo(teacherContent).offset(-6)
+            maker.top.equalTo(assistantAvatar.snp.centerY)
         }
         courseDateLabel.snp.makeConstraints { (maker) in
-            maker.centerY.equalTo(dateIcon).offset(MalaScreenOnePixel)
-            maker.left.equalTo(dateIcon.snp.right).offset(4)
-            maker.height.equalTo(13)
+            maker.height.equalTo(17)
+            maker.bottom.equalTo(content).offset(-10.5)
+            maker.left.equalTo(content).offset(11)
         }
         gradeLabel.snp.makeConstraints { (maker) in
-            maker.top.equalTo(courseContent).offset(15)
-            maker.right.equalTo(courseContent).offset(-12)
-            maker.height.equalTo(courseName)
+            maker.left.equalTo(courseDateLabel.snp.right).offset(10)
+            maker.right.equalTo(priceLabel.snp.left).offset(-10)
+            maker.height.equalTo(17)
+            maker.bottom.equalTo(content).offset(-10.5)
         }
         lessionsLabel.snp.makeConstraints { (maker) in
-            maker.right.equalTo(gradeLabel)
-            maker.bottom.equalTo(priceLabel)
+            maker.right.equalTo(content).offset(-12)
+            maker.bottom.equalTo(content).offset(-12)
             maker.height.equalTo(14)
         }
         priceLabel.snp.makeConstraints { (maker) in
-            maker.top.equalTo(gradeLabel.snp.bottom).offset(12)
             maker.right.equalTo(lessionsLabel.snp.left)
-            maker.bottom.equalTo(courseContent).offset(-12)
+            maker.centerY.equalTo(lessionsLabel)
             maker.height.equalTo(20)
         }
     }
